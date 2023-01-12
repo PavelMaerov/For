f9ae3321f63afca2328516431391611d4d251c16
asus <123456>
2023-01-12 03:39:24 +0300

    Коммит с задачей 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //С помощью цикла for выведите в консоль все числа от 1 до 10
+        System.out.println("Задача 1");
+        for (int i=1;i<=10;i++) {System.out.println(i);}
     }
 }
